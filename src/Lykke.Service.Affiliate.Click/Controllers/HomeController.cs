d5bd16e62a4d7f1c477b2c93ceab458dd923bf1a
Roman Chuprikov <paradise.develop@gmail.com>
2018-02-21 02:21:26 +0500

    add headers logs

--- src/Lykke.Service.Affiliate.Click/Controllers/HomeController.cs
+++ src/Lykke.Service.Affiliate.Click/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Common.Log;
 using Lykke.Common.Extensions;
 using Lykke.Service.Affiliate.Click.Settings.ServiceSettings;
 using Lykke.Service.Affiliate.Core.Domain.Repositories.Mongo;
@@ -12,12 +14,14 @@ namespace Lykke.Service.Affiliate.Click.Controllers
         private readonly ILinkRepository _linkRepository;
         private readonly ILinkRedirectRepository _linkRedirectRepository;
         private readonly RedirectIpCacheSetting _setting;
+        private readonly ILog _logger;
 
-        public HomeController(ILinkRepository linkRepository, ILinkRedirectRepository linkRedirectRepository, RedirectIpCacheSetting setting)
+        public HomeController(ILinkRepository linkRepository, ILinkRedirectRepository linkRedirectRepository, RedirectIpCacheSetting setting, ILog logger)
         {
             _linkRepository = linkRepository;
             _linkRedirectRepository = linkRedirectRepository;
             _setting = setting;
+            _logger = logger;
         }
 
         [HttpGet("{key}")]
@@ -29,9 +33,25 @@ namespace Lykke.Service.Affiliate.Click.Controllers
             if (redirectLink == null)
                 return NotFound();
 
+            await LogHeaders();
+
             await _linkRedirectRepository.SaveRedirect(HttpContext.GetIp(), redirectLink.AffiliateId, key, _setting.IpCacheTime);
-            
+
             return Redirect(redirectLink.RedirectUrl);
+        }
+
+        private async Task LogHeaders()
+        {
+            try
+            {
+                var values = string.Join(",", HttpContext.Request.Headers.Values.Select(x => x.ToString()));
+                var keys = string.Join(",", HttpContext.Request.Headers.Keys);
+                await _logger.WriteInfoAsync(nameof(RegisterLink), values, keys);
+            }
+            // ReSharper disable once EmptyGeneralCatchClause
+            catch (System.Exception)
+            {
+            }
         }
     }
 }
