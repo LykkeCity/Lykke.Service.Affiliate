1cfd900fe09d739d7852e6ba59348dafd508215d
Dmitry Samodov <samodov.dmitry@gmail.com>
2018-10-17 13:47:11 +0300

    removed managers registration from click service

--- src/Lykke.Service.Affiliate.Click/Modules/ServiceModule.cs
+++ src/Lykke.Service.Affiliate.Click/Modules/ServiceModule.cs
@@ -1,12 +1,8 @@
 ﻿using Autofac;
-using Common.Log;
-using Lykke.Sdk;
 using Lykke.Service.Affiliate.Click.Settings;
-using Lykke.Service.Affiliate.Click.Settings.ServiceSettings;
 using Lykke.Service.Affiliate.Core.Domain.Repositories.Mongo;
 using Lykke.Service.Affiliate.MongoRepositories.Mongo;
 using Lykke.Service.Affiliate.MongoRepositories.Repositories;
-using Lykke.Service.Affiliate.Services.Managers;
 using Lykke.SettingsReader;
 using MongoDB.Driver;
 
@@ -24,12 +20,6 @@ namespace Lykke.Service.Affiliate.Click.Modules
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterInstance(_settings.CurrentValue.AffiliateClickService.RedirectIpCacheSetting);
-
-            builder.RegisterType<StartupManager>()
-                .As<IStartupManager>();
-
-            builder.RegisterType<ShutdownManager>()
-                .As<IShutdownManager>();
 
             BuildRepositories(builder);
         }
